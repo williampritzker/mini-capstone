e365ca89ab918d02b1200ef97fec50fcc809f929
will <williampritzker@gmail.com>
2018-08-29 13:47:37 -0500

    adding products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# product = Product.new(name: "Whey Protein", price: 20, image_url: "https://i5.walmartimages.com/asr/f07bfd12-4438-4c20-8ce1-a830e18ba86e_1.9edc38b5bcc107b433cafcbc563d779a.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", description: "100% Whey Protein Isolate")
+# product.save
+
+# product = Product.new(name: "Creatine", price: 15, image_url: "http://www.gnc.com/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog-gnc/default/dw07bea2c2/hi-res/350140_1.jpg?sw=2000&sh=2000&sm=fit", description: "Micronized Creatine Powder")
+# product.save
+
+# product = Product.new(name: "C4 Pre-Workout", price: 17, image_url: "https://cdn.shopify.com/s/files/1/1492/2278/t/3/assets/nav-c4-original.png?3061291216469337077", description: "The Original Explosive Pre-Workout")
+# product.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
